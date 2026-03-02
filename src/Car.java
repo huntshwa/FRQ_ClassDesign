c3f8eac8d0663706117c38e1138c9fa7aaec4c62
HunterYarmy <huntery5@nycstudents.net>
2026-03-02 15:34:52 -0500

    initial push

--- src/Car.java
+++ src/Car.java
@@ -1,4 +1,19 @@
-package PACKAGE_NAME;
-
 public class Car {
+    private double fuel;
+    private double milesPerGallon;
+
+    public Car(double fuel, double mpg) {
+        this.fuel = fuel;
+        milesPerGallon = mpg;
+    }
+
+    public boolean canMakeTrip(double distance) {
+        if (milesPerGallon * fuel >= distance) {
+            fuel -= distance / milesPerGallon;
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,14 @@
-package PACKAGE_NAME;
-
 public class Main {
+    public static void main(String[] args) {
+        boolean possible;
+        Car carOne = new Car(10.0, 30.0);
+        possible = carOne.canMakeTrip(120.0);
+        possible = carOne.canMakeTrip(90.0);
+        possible = carOne.canMakeTrip(225.0);
+        Car carTwo = new Car(12.0, 15.0);
+        possible = carTwo.canMakeTrip(300.0);
+        possible = carTwo.canMakeTrip(180.0);
+
+
+    }
 }
